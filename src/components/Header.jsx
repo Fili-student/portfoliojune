f764fa7cc9a2b1b1df27e70208685ec016463ef5
Fili-student <triantafillia.filippou@dci-student.org>
2024-06-17 17:03:06 +0200

    mo

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+function Header(){
+    return(
+        <>
+
+        <div className="oben">
+        <section className="headersec">
+        <a href="">
+        <i class="fa-solid fa-envelope oben-i"></i></a>
+        </section>
+         </div>
+        </>
+    );
+}
+
+export default Header
+
+
+// vielleicht logo oder drawing noch?
---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+function Hero(){
+    return(
+        <>
+        <div className="hero">
+            <section className="herosec">
+            <h1 className="port">Portfolio</h1>
+            {/* <img src="" alt="" /> */}
+            </section>
+        </div>
+        </>
+    );
+}
+export default Hero
